743dafb1b6849353d6ba26cfc9b0ff109a9d3d28
Justin Hawkins <justin@hawkins.id.au>
2023-03-05 09:18:47 +1030

    Better comments

--- main.go
+++ main.go
@@ -6,17 +6,21 @@ import (
 )
 
 func main() {
+	// create a migrator, connecting to the postgresql
+	// database defined by the environment variable
 	migrator, err := migrations.NewMigrator(os.Getenv("DB_DNS"))
 	if err != nil {
 		panic(err)
 	}
 
+	// get the current migration status
 	now, exp, info, err := migrator.Info()
 	if err != nil {
 		panic(err)
 	}
 	if now < exp {
-		// migration is required, dump out the current state and perform the migration
+		// migration is required, dump out the current state
+		// and perform the migration
 		println("migration needed, current state:")
 		println(info)
 
